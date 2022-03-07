1b9c0102674b15042277481f03b0859a34fbc54f
VWaghray <87341415+VWaghray@users.noreply.github.com>
2022-03-07 17:19:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,19 @@ function preload(){
     classifier=ml5.imageClassifier('DoodleNet');
 }
 
+functiom draw {
+    strockWeight(13);
+    stroke=0;
+    if (mouseIsPressed) { line(pmouseX, pmouseY, mouseX, mouseY); 
+}
+
+    function classifyCanvas() { 
+        classifier.classify(canvas, gotResult); 
+    }
+    
+    function gotResult(error, results)
+    { if (error) { console.error(error); }
+     
+     console.log(results); drawn_sketch = results[0].label; document.getElementById('label').innerHTML = 'Your Sketch: ' + drawn_sketch; document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';}
+    
+    
